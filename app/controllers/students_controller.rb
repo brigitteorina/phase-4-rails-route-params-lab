8d1b3e05f53eede25ccdaa3d2e3b7f8b0506d784
brigitteorina <brigitteorina@gmail.com>
2023-06-21 21:05:28 +0300

    completed assignment

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,34 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+  def grades
+    students = Student.order(grade: :desc)
+    render json: students
+  end
+
+  def highest_grade
+    student = Student.order(grade: :desc).first
+    render json: student
+  end
+
+
+  #The user is able to go to a URL with the student's ID (i.e. /students/2) to receive information on the student.
+  #The application should render the values from the student record with the corresponding ID in the database.
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
+
+
+  #Use query params in the index action to return a list of all students whose first or last name matches the query parameter
+  def index
+    if params[:name]
+      students = Student.where("first_name LIKE ? OR last_name LIKE ?", "%#{params[:name]}%", "%#{params[:name]}%")
+    else
+      students = Student.all
+    end
+    render json: students
+  end
+
+
 end
